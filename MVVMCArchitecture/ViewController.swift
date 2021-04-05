8ecc90bcf5aab9914fa3497c227633889a9df2ae
raul-air <72030043+raul-air@users.noreply.github.com>
2021-04-05 09:41:08 -0300

    Initial Commit

---
+++ MVVMCArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMCArchitecture
+//
+//  Created by RAirApps on 05/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
